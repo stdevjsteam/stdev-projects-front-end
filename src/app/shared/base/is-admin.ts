b9e4a73e4c4f9bc6e01ff522f54a586bddbe6d00
stdevjsteam <stdevjsteam@gmail.com>
2018-11-18 16:10:32 +0400

    fix

--- src/app/shared/base/is-admin.ts
+++ src/app/shared/base/is-admin.ts
@@ -1,7 +1,7 @@
 import { AuthService } from '../services/auth.service';
 
 export abstract class IsAdmin {
-    protected isAdmin = false;
+    public isAdmin = false;
 
     constructor(protected authService: AuthService) {
         this.isAdmin = this.authService.decodeToken().isAdmin;
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ProjectsTableComponent } from './components/projects-table/projects-table.component';
-import { ManageTableComponent } from './components/manage-table/manage-table.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    ProjectsTableComponent,
-    ManageTableComponent
+    ProjectsTableComponent
   ],
   exports: [
     ProjectsTableComponent,
